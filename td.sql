fd0737117c72df876a0b0f23b3e9eb9d0a0e697f
PirateX <shantam2@gmail.com>
2018-01-25 13:46:20 +0530

    Create td.sql

---
+++ td.sql
@@ -0,0 +1,55 @@
+------Different kind of StatementTypes in Teradata 15.1.0
+
+select distinct statementtype  FROM PDCRINFO.DBQLOGTBL;
+/*
+StatementType
+Merge Into          
+Release Lock        
+Begin Loading       
+Modify Database     
+Grant               
+Abort               
+Check Workload      
+Collect Statistics  
+Set Session         
+Replace View        
+Mload               
+Drop Index          
+CheckPoint Loading  
+Delete              
+End Edit            
+Drop Statistics     
+End Loading         
+Create Database/User
+Show                
+Unrecognized type   
+Drop Macro          
+Update              
+UCAbort             
+End Transaction     
+Create Table        
+Set Query_Band      
+Revoke              
+Create View         
+Exec                
+Select              
+Drop Table          
+Begin/End DBQL      
+Insert              
+Begin Transaction   
+Grant/Revoke Logon  
+Rename Table        
+Comment Get/Set     
+Alter Table         
+Help                
+Drop View           
+Begin Mload         
+Replace Macro       
+Create Macro        
+Database            
+Commit Work         
+Null                
+Call                
+Give                
+Execute Mload
+*/
